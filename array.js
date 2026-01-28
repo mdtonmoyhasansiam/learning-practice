becd336c38012b4bce955ff4b0bbb1dbfb62cd5f
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-01-28 16:45:47 +0600

    Explore Array

---
+++ array.js
@@ -0,0 +1,9 @@
+const numbers = [12, 15, 50, 60, 70]; // Number Array
+
+const sigarate = ["Banson", "Hollywood", "Goldlief", "Advanced"]; // String Array
+
+const failed = [true, false, true, true]; // Boollean Array
+
+const mixedArray = ["Derby", true, 21, 0.1]; // mixedArry
+
+console.log(mixedArray); // Array Output
