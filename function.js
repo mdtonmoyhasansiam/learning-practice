956af4ea739a09545bfc0a58b7abb6f4c9830483
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-01 12:34:06 +0600

    learning function

---
+++ function.js
@@ -0,0 +1,9 @@
+// Function declaretion
+
+function openVScode (){
+    console.log("Open the PC");
+    console.log("Then go to windows search bar");
+    console.log("Search VS code, click and open the VS dashboard");
+}
+
+openVScode ();
