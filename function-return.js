662671d8867288c5f5f6775c3f623c0933cea04c
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-01 14:23:59 +0600

    Function return explore

---
+++ function-return.js
@@ -0,0 +1,10 @@
+function tenTimes(number){
+    const result = number * 3;
+    // console.log(result);
+    return result;
+}
+
+tenTimes (10);
+
+const bigNumber = tenTimes(20);
+console.log("bigNumber", bigNumber);
