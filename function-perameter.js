12697a97f2acb0f89c9d4c534fb807745c804dbc
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-01 13:26:07 +0600

    Subtraction

--- function-perameter.js
+++ function-perameter.js
@@ -1,9 +1,13 @@
+// বর্গ প্যারামিটার
+
 function squar (number){
     const borgo = number * number;
     console.log(borgo);    
 }
 
 squar (505);
+
+// যোগ প্যারামিটার
 
 function add (a, b){
     const sum = a + b;
@@ -18,3 +22,12 @@ function addMultiple (x, y, z){
 }
 
 addMultiple(25, 65, 80);
+
+// বিয়োগ প্যারামিটার
+
+function Subtraction (z, x){
+    const biyog = z-x;
+    console.log(biyog);
+}
+
+Subtraction(875, 874);
