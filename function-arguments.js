d9830d362fc4a7b38315aca21ea5fdfa29a09264
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-01 13:57:08 +0600

    Argument

--- function-arguments.js
+++ function-arguments.js
@@ -1,6 +1,12 @@
 function doubleNum (a, b){
     const double = a+b;
-    console.log(double, "Individual is", a, b);
+    console.log(double, "Individual is", a);
 }
 
 doubleNum (10, 20);
+console.log("------------------")
+const number = 120+10;
+doubleNum (number);
+console.log("-------------------")
+let money = 1020+20;
+doubleNum (money);
