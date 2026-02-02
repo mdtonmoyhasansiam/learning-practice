1e12aae17b4217ab8157171fde9c6373c89bde2b
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-02 16:23:19 +0600

    Function conversion

--- function-conversion.js
+++ function-conversion.js
@@ -1,3 +1,5 @@
+//Convert KM to Meter
+
 function kmTometer(km){
     const meter = km * 1000;
     return meter;
@@ -5,3 +7,26 @@ function kmTometer(km){
 
 const exactMeter = kmTometer(12);
 console.log(exactMeter);
+
+// Convert CM to Meter
+
+function cmTometer(cm){
+    const meter = cm * 0.01;
+    return meter;
+}
+
+const exact_meter = cmTometer (10);
+console.log(exact_meter);
+
+// Convert inch to feet
+
+function inchTofeet(inch){
+    const feet = inch * 0.0833333;
+    // return feet; [Way 1]
+    console.log(feet); // [Way 2]
+}
+
+// const exactFeet = inchTofeet(110); [Way 1]
+// console.log(exactFeet); [Way 1]
+
+inchTofeet(110); // [Way 2]
