39d9541fadf17a1911f72d61171d725334c8cba0
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-03 17:02:58 +0600

    Added find out max and min numbers

--- find-out-max-min-numbers.js
+++ find-out-max-min-numbers.js
@@ -1,3 +1,5 @@
+// Find out minimum numbers
+
 const height = [12, 20,78,52, 2, 5, 7, 105];
 
 function minimum (numbers){
@@ -12,3 +14,20 @@ function minimum (numbers){
 
 const min = minimum(height);
 console.log(min);
+
+// Find out maximum numbers
+
+const sNumber = [10, 25, 70, 20, 21, 52, 65, 40,05, 03, 20,80,98,50, 40];
+
+function maximum(numbers){
+    let max = numbers[0];
+    for(const number of numbers){
+        if(number > max){
+            max = number;
+        }
+    }
+    return max
+}
+
+const max = maximum(sNumber);
+console.log(max);
