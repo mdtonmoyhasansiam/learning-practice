6581d70dc36728ad30745d58bbe56ad56ae9089f
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-03 13:38:01 +0600

    Find out get strawberry

---
+++ find-out-get-strawberry.js
@@ -0,0 +1,12 @@
+// Way 01
+
+const kabul = 65;
+const cabul = 70;
+
+if(kabul > cabul){
+    console.log("kabul will get the strawberry");
+}
+
+else{
+    console.log("cabul will get the strawberry");
+}
