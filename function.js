571356de922e53d3d5123246ea5153952e3d4405
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-06 22:14:23 +0600

    sum concept added

--- function.js
+++ function.js
@@ -15,3 +15,23 @@ function openInstagram (){
 }
 
 openInstagram ();
+
+
+
+// Problem-00: Sample(You don't need to solve this!)
+// ⚠️ Function Name Must be add()
+// এটা একটা Sample Problem! তোমাকে এই Problem টি Solve করতে হবে না। তুমি যদি solve করো তাতেও সমস্যা নেই। এই Problem এর জন্য কোনো মার্ক দেয়া হবে না এবং মার্ক কাটাও হবে না।
+
+// দুটি সংখ্যা ইনপুট হিসেবে নিয়ে তাদের যোগফল return করবে এমন একটি ফাংশন লিখো।
+
+// 📥 Input:
+// Two numbers: a : Number, b : Number
+// 🚀 Output:
+// The sum of the two input numbers (a + b : Number)
+// 💡 Hints:
+// Use + operator or Math.add() function
+
+function add(a, b) {
+    let sum =a+b;
+    return sum;
+}
